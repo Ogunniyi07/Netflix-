65de0590d79063a5b25b136caa58dff15dd7b5ae
Ogunniyi07 <73582483+Ogunniyi07@users.noreply.github.com>
2022-03-11 07:59:57 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ window.onscroll = function(){
         nav.classList.remove("sticky");
     }
 }
+
+
+//changing banner image at intervals
+   /*function changeBg(){
+
+        const images = [
+        'url("2.jpg")',
+        'url("3.jpg")',
+        'url("4.jpg")',
+        'url("5.jpg")',
+        'url("6.jpg")',
+        ]
+   
+
+    const banner = document.querySelector('.home .banner')
+    const bg = images[Math.floor(Math.random() * 
+       images.length )];
+       banner.style.backgroundImage = bg;
+
+    }
+    setInterval(changeBg, 5000)*/
+
+
+
+    window.onload = function () {
+        var images = ['2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
+        var image = images[Math.floor(Math.random() * images.length)];
+        document.getElementsByTagName('.home .banner')[0].style.backgroundImage = "url('" + image + "')";
+        document.getElementsByTagName('body')[0].style.backgroundRepeat = "no-repeat";
+        document.getElementsByTagName('body')[0].style.backgroundSize = "cover";
+    }
+
+
+  
